894fa78892f6b49adb436557b1841da28f5e49b2
gordonm <9099488+gordonm867@users.noreply.github.com>
2020-10-18 10:28:15 -0400

    Update Vision.java

--- Tests/Vision.java
+++ Tests/Vision.java
@@ -31,7 +31,7 @@ public class Vision extends LinearOpMode {
     public static int lefty = 140;
     public static int righty = 180;
 
-    public static boolean ready = true;
+    public static boolean ready = tru e;
 
     public void runOpMode() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
