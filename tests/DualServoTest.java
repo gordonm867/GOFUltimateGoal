88504a18ec66e2329c9364ad29f98fc32911dfa1
ItchyCatIII <ItchyCatIII@users.noreply.github.com>
2021-03-25 22:07:56 -0400

    Update DualServoTest.java

--- tests/DualServoTest.java
+++ tests/DualServoTest.java
@@ -6,6 +6,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.gofultimategoal.globals.GOFException;
+import org.firstinspires.ftc.teamcode.gofultimategoal.globals.Globals;
 import org.firstinspires.ftc.teamcode.gofultimategoal.hardware.GOFHardware;
 import org.firstinspires.ftc.teamcode.gofultimategoal.util.MyOpMode;
 
@@ -20,6 +21,8 @@ public class DualServoTest extends MyOpMode {
     public static double servo1pos = 0.5;
     public static double servo2pos = 0.5;
     public static boolean paused = false;
+    public static boolean intake = false;
+    public static boolean outtake = false;
 
     @Override
     public void initOp() throws InterruptedException, GOFException {
@@ -32,6 +35,15 @@ public class DualServoTest extends MyOpMode {
         if(!paused) {
             robot.d1.setPosition(servo1pos);
             robot.d2.setPosition(servo2pos);
+        }
+        if(intake) {
+            robot.setIntakePower(-Globals.MAX_IN_SPEED);
+        }
+        if(outtake) {
+            robot.setIntakePower(Globals.MAX_IN_SPEED);
+        }
+        if(!intake && !outtake) {
+            robot.setIntakePower(0);
         }
         telemetry.addData("d1", robot.d1.getPosition());
         telemetry.addData("d2", robot.d2.getPosition());
